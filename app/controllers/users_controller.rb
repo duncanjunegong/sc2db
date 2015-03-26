594255fd4dde4bfa8531f6820fe5c87a26ca1587
Jianhui Gong <gongjianhui@genomics.cn>
2015-03-26 21:47:07 +0800

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "You can use sc2db now!"
       redirect_to @user 
     else
